2dea4f3399ecb8228181cf44ff5b28f193d190b6
Dave879 <dvd.rori879@gmail.com>
2024-11-26 13:28:24 +0100

    Added input from file, refactoring, updated README

--- include/LidarDriver.h
+++ include/LidarDriver.h
@@ -1,10 +1,9 @@
 #ifndef LDP_INTERMEDIO_INCLUDE_LIDARDRIVER_H_
 #define LDP_INTERMEDIO_INCLUDE_LIDARDRIVER_H_
 
+#include <vector>
 #include <ostream>
-#include <vector>
 #include <cmath>
-#include <vector>
 
 class LidarDriver
 {
@@ -16,39 +15,39 @@ class LidarDriver
     std::vector<std::vector<double>> scans_;
 
 public:
-    /// \brief Istanzia la classe LidarDriver
-    /// \param resolution La risoluzione angolare del sensore in gradi
+    /// Instances the class LidarDriver
+    /// \throws InvalidResolution If the resolution is out of the range [0.1, 1]
+    /// \param resolution The angular resolution of the sensor in degrees
     explicit LidarDriver(const double resolution = 1);
 
-    /// Accetta uno std::vector<double> contenente una scansione e lo memorizza
-    /// nel buffer (sovrascrivendo la scansione meno recente se il buffer è pieno).
-    /// Questa funzione esegue anche il controllo di dimensione: se le letture sono
-    /// in numero minore del previsto, completa i dati mancanti a 0; se sono in
-    /// numero maggiore, li taglia.
+    /// Accepts a std::vector<double> containing a scan and stores it in the buffer
+    /// (overwriting the oldest scan if the buffer is full).
+    /// This function also checks the size of the buffer: if the readings are less
+    /// than expected, it completes missing data to 0, if they are more they get
+    /// discarded.
     /// \param scan_to_store Dati da memorizzare nel buffer interno
     void new_scan(const std::vector<double> &scan_to_store);
 
-    /// Fornisce la scansione più vecchia e la rimuove dal buffer
-    /// \returns La scansione più vecchia del sensore
+    /// Retrieves the oldest scan and removes it from the internal buffer
+    /// \throws BufferEmptyError If the buffer is empty
+    /// \returns The oldest scan
     std::vector<double> get_scan();
 
-    /// Elimina tutte le scansioni salvate
+    /// Deletes all stored scans
     void clear_buffer();
 
-    /// Accetta un angolo espresso in gradi e ritorna la lettura corrispondente a
-    /// tale angolo nella scansione più recente acquisita dal sensore; tale scansione
-    /// non è eliminata dal buffer, e se l’angolo richiesto non è disponibile ritorna
-    /// la lettura relativa al valore di angolo più vicino
-    /// \param degree L'angolo
-    /// \throws BufferEmptyError Se il buffer è vuoto
-    /// \throws InvalidDegree Se l'angolo è fuori dal range [0, 180]
-    double get_distance(const double degree) const;
+    /// Accepts an angle in degrees and returns the reading that corresponds to
+    /// that angle in the most recent reading from the sensor.
+    /// This reading doesn't get deleted from the buffer.
+    /// If the reading doesn't get deleted from the buffer and the requested angle
+    /// isn't available it returns the reading of the closest angle.
+    /// \throws BufferEmptyError If the buffer is empty
+    /// \param angle The angle in degrees
+    double get_distance(const double angle) const;
 
-    /// Stampa a schermo l'ultima scansione salvata ma non la rimuove dal buffer
+    /// Prints the most recent scan saved, it doesn't get removed from the buffer
     friend std::ostream &operator<<(std::ostream &os, const LidarDriver &l);
 
-    // E' stata tentata un operazione che richiedeva la presenza di elementi nel 
-    // buffer quando il buffer era ancora vuoto
     class BufferEmptyError : public std::exception {
         virtual const char* what() const noexcept
         {
@@ -56,15 +55,6 @@ public:
         }
     };
 
-    // E' stato immesso un angolo troppo piccolo o grande (minore di 0 oppure 
-    // maggiore di 180)
-    class InvalidAngle: public std::exception {
-        virtual const char* what() const noexcept
-        {
-            return "[InvalidAngle] Exception: Angle is out of the range [0, 180]";
-        }
-    };
-    // La risoluzione immessa non è tra 0.1° e 1°
     class InvalidResolution: public std::exception {
         virtual const char* what() const noexcept
         {
--- src/main.cpp
+++ src/main.cpp
@@ -9,38 +9,48 @@ int main(int argc, char *argv[])
 {
     LOG("Starting LidarDriver");
 
-    // TODO: file input
-    if (argc <= 1)
+    if (argc > 1) // If input file path is present
     {
-    	LOG("Error! An input filename must be specified: ./ldp-intermedio input.txt");
-    	return 1;
-    }
+        std::vector<double> numbers_from_file;
+        std::ifstream file(argv[1]);
 
-    std::ifstream file(argv[1]);
+        if (!file.is_open())
+        {
+            LOG("Error reading the file \"" << argv[1] << "\"!");
+        }
+        else
+        {
+            LOG("Parsing file " << argv[1]);
+        }
 
-    if (!file.is_open())
-    {
-    	LOG("Error reading the file \"" << argv[1] << "\"!");
-    }
-    else
-    {
-    	LOG("Parsing file " << argv[1]);
-    }
+        std::string str;
 
-    std::string str;
+        while (std::getline(file, str))
+        {
+            try
+            {
+                numbers_from_file.push_back(std::stod(str));
+            }
+            catch(const std::exception& e)
+            {
+                std::cerr << "Couldn't convert \"" << str << "\" " << "to double" << '\n';
+            }
+        }
 
-    while (std::getline(file, str))
-    {
-        // TODO
-    }
+        if (file.bad())
+        {
+            LOG("Error while reading file");
+        }
 
-    if (file.bad())
-    {
-    	LOG("Error while reading file");
+        LidarDriver lidar(0.9);
+        lidar.new_scan(numbers_from_file);
+        LOG("Latest scan parsed from file:");
+        LOG(lidar);
     }
 
     // Initialize LidarDriver
-    LidarDriver l = LidarDriver(5);
+
+    LidarDriver l;
     LOG("1. Initialized LidarDriver object (without any new_scan):");
     LOG(l);
     LOG("================================================================================\n");
@@ -63,6 +73,7 @@ int main(int argc, char *argv[])
     LOG("================================================================================\n");
 
     // Perform third new_scan with an even larger vector
+    bu1.clear();
     for (int i = 1; i < 250; i++)
     {
         bu1.push_back(i);
@@ -72,7 +83,6 @@ int main(int argc, char *argv[])
     LOG(l);
     LOG("================================================================================\n");
 
-    // Retrieve and print the oldest scan
     LOG("5. Retrieving the oldest scan:");
     std::vector<double> v = l.get_scan();
     LOG("Oldest scan vector:");
@@ -83,38 +93,34 @@ int main(int argc, char *argv[])
     LOG("");
     LOG("================================================================================\n");
 
-    // Check the buffer after retrieving the oldest scan
-    LOG("6. After one get_scan call (oldest scan removed):");
+    LOG("6. Most recent scan is still the same after removal:");
     LOG(l);
     LOG("================================================================================\n");
 
-    LOG("7. Getting a specific distance for angle = 90.3 (nearest valid degree = 90.5):");
-    LOG(l.get_distance(90.3));
-    LOG("================================================================================\n");
-
-    LOG("8. Getting a specific distance for invalid angle = 400 (expected InvalidAngle)");
-    try
-    {
-        LOG(l.get_distance(400));
-    }
-    catch (const std::exception &e)
-    {
-        std::cerr << e.what() << '\n';
-    }
+    LOG("7. Getting a specific distance for angle:");
+    LOG("-100°: " << l.get_distance(-100));
+    LOG("0°: " << l.get_distance(0));
+    LOG("10°: " << l.get_distance(10));
+    LOG("10.2°: " << l.get_distance(10.2));
+    LOG("10.5°: " << l.get_distance(10.5));
+    LOG("10.55°: " << l.get_distance(10.55));
+    LOG("10.9°: " << l.get_distance(10.9));
+    LOG("90.3°: " << l.get_distance(90.3));
+    LOG("300°: " << l.get_distance(300));
     LOG("================================================================================\n");
 
     // Clear the buffer and verify
-    LOG("9. Clearing the buffer:");
+    LOG("8. Clearing the buffer:");
     l.clear_buffer();
     LOG("After clearing the buffer:");
     LOG(l);
     LOG("================================================================================\n");
 
-    // Attempt to get a distance from an empty buffer
-    LOG("10. Attempting to get distance with an empty buffer (expected BufferEmptyError):");
+
+    LOG("9. Getting a specific distance for when buffer is empty (Expected BufferEmptyError):");
     try
     {
-        LOG(l.get_distance(23.4));
+        LOG(l.get_distance(42));
     }
     catch (const std::exception &e)
     {
@@ -122,21 +128,26 @@ int main(int argc, char *argv[])
     }
     LOG("================================================================================\n");
 
-    // Fill the buffer with multiple scans
-    LOG("11. Filling the scan buffer:");
-    l.new_scan(std::vector<double>{1, 2});
+    LOG("10. Filling the scan buffer:");
+    l.new_scan({1, 2});
+    LOG("Added {1, 2}");
+    LOG(l);
     std::vector<double> bu2 = {1, 2, 3, 4, 5, 6};
     l.new_scan(bu2);
+    LOG("Added {1, 2, 3, 4, 5, 6}");
+    LOG(l);
     for (int i = 1; i < 270; i++)
     {
         bu2.push_back(i);
     }
     for (int i = 0; i < 8; i++)
     {
-        double tmp = bu2[i]; 
-        bu2[i] = (i+7)*1000;
+        double tmp = bu2[i];
+        bu2[i] = (i + 7) * 1000;
         l.new_scan(bu2);
-        bu2[i] = std::move(tmp);
+        LOG(i << " Added bu2 vector");
+        bu2[i] = tmp;
+        LOG(l);
     }
     LOG("================================================================================\n");
 
@@ -149,12 +160,26 @@ int main(int argc, char *argv[])
 
     // Remove all scans one by one and print them
     LOG("13. Removing all scans from the buffer:");
-    for (size_t i = 0; i < 10; i++) {
+    for (size_t i = 0; i < 10; i++)
+    {
         LOG(l);
         l.get_scan();
     }
     LOG("After removing all elements:");
     LOG(l);
+    LOG("================================================================================\n");
+
+    // Remove all scans one by one and print them
+    LOG("14. Should throw InvalidResolution exception:");
+    try
+    {
+        LidarDriver l1 = LidarDriver(1000);
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    LOG("================================================================================\n");
 
     LOG("LidarDriver tests completed.");
 
